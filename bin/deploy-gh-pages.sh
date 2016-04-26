7f8f667e1666aeff79ec5c6e0842463529af87e1
Jaco Greeff <jacogr@gmail.com>
2016-04-26 17:07:48 +0200

    checkout gh-pages, merge master

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -8,10 +8,15 @@ fi
 # our user config
 git config user.name "Travis CI"
 git config user.email "jacogr+travis@gmail.com"
+git config --global push.default simple
+
+# checkout the gh-pages branch
+git checkout gh-pages
+git merge master
 
 # add the build arttifacts
 git add --force client.css client.js index.html
 git commit -m "[CI skip] push to gh-pages"
 
 # send it up to the gh-pages branch
-git push --force --quiet "https://${GH_TOKEN}@github.com/jacogr/ethcore-jacogr.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/jacogr/ethcore-jacogr.git" gh-pages > /dev/null 2>&1
