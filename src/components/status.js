31fbf87ca504a39c690efe85dc59b62111e9a0e8
Jaco Greeff <jacogr@gmail.com>
2016-04-27 11:34:45 +0200

    expanded status

--- src/components/status.js
+++ src/components/status.js
@@ -1,14 +1,25 @@
 module.exports = {
-  'essentials': {
-    'locations': 'Interested in the German location, please see remote location comments later on as well as my whys for looking around.',
-    'startdate': 'As soon as possible is great, I do however have a 2 month notice period. As they say, good things come to those who wait.',
-    'salary': 'Without beating around the bush, this is quite a it below my range and experience. Negotiation is on the table, I know that I can bring more than you planned for, resulting in a win-win. What about some part in ETH? (Saves me investing each month via the Fiat -> BTC -> ETH bridges)',
-    'industry': 'I have been involved with Bitcoin as an buyer since late 2011 (having dimissed it off the bat in 2010). As for Ethereum, actually stumbled across it only in January 2016 after I wanted to write some Bitcoin gambling site... Well, I have been hooked since, contracts, interfaces - not enough to satisfy the learning appetite, but enough to keep me sane.',
-    'companysize': 'Brilliant. Be small, move fast, do things that matter.'
+  essentials: {
+    locations: 'Interested in the German location, please see remote location comments later on as well as to my whys for looking around.',
+    startdate: 'As soon as possible is great, I do however have a 2 month notice period. As they say, good things come to those who wait.',
+    salary: 'Without beating around the bush, this is quite a it below my range and experience. Negotiation is on the table, I know that I can bring more than you planned for, resulting in a win-win. What about some part in ETH? (Saves me investing each month via the Fiat -> BTC -> ETH bridges)',
+    industry: 'I have been involved with Bitcoin as an buyer since late 2012 (having dimissed it totally in 2010). As for Ethereum, actually stumbled across it only in Jan 2016 after I wanted to write some Bitcoin gambling site... Well, I have been hooked since, contracts, interfaces - not enough to satisfy the learning appetite, but enough to keep me sane.',
+    companysize: 'Brilliant. Be small, move fast, do things that matter.'
   },
-  'methodology': {
-    'buildserver': 'Have a lot of experience with CircleCI (slightly cheaper for paid accounts than Travis), however with this one took the Travis route. Slightly different syntaxin the yml config, however a CI is a CI...',
-    'staticcodeanalysis': 'For JavaScript CodeClimate does work wonders, although (by default) they rely on jshint as opposed to eslint (their engine config is not quite optimal yet for the latter). Combine that with a local ESLint and coding can be checked for the simpler issues.',
-    'onecommandbuild': 'Unfortunately too many in this space leave out rigour when building, yes, a proper build (HTML, JS, CSS, etc.) is critical. Very happy to see this here.'
+  methodology: {
+    buildserver: 'Have a lot of experience with CircleCI (slightly cheaper for paid accounts than Travis), however with this project took the Travis route. Slightly different syntaxin the yml config, however a CI is a CI...',
+    staticcodeanalysis: 'For JavaScript CodeClimate does work wonders, although (by default) they rely on jshint as opposed to eslint (their engine config is not quite optimal yet for the latter). Combine that with a local ESLint and coding can be checked for the simpler issues.',
+    onecommandbuild: 'Unfortunately too many in this space leave out rigour when building, yes, a proper build (HTML, JS, CSS, etc.) is critical. Very happy to see this here.'
+  },
+  equipment: {
+    operatingsystem: 'Currently a Mac OS X user. Prior to that have been using Linux as a desktop OS since 1996, however when I finally got my hands on a Mac Mini G5 with BSD under-the-hood, I have not looked back.',
+    computer: 'Thin, light, portable with enough memory and a long battery life.'
+  },
+  technologies: {
+    css3: 'I can hold my own.',
+    html5: '',
+    javascript: 'We have come a long way and I would have never expected myself to use JS on both the server and the client, but here we are. It does have its moments, provided you apply some rigour and ES2015 has definately helped.',
+    node: 'Yes. Current propritary projects are all a combination of client/server JS, with a mixture of code-sharing.',
+    frameworks: 'Biggest strength is on AngularJS (1.x, no production 2.x experience) and have been using Polymer and React (like here) lately to expand my horizons. Apart from my initial JSX knee-jerk, the latter has been quite good, not without its warts, but productive. Familiar with Meteor, however have never viewed it as a real-contender, rather as a slightly-less attractive approach.'
   }
 };
