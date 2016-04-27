9676751c0dce143dfde36b35b0737808e87af6f9
Jaco Greeff <jacogr@gmail.com>
2016-04-27 19:06:26 +0200

    expand comments

--- src/constants/comments.js
+++ src/constants/comments.js
@@ -1,15 +1,22 @@
 module.exports = {
   essentials: {
-    locations: 'Interested in the German location, please see remote location comments later on as well as to my whys for looking around.',
+    locations: 'Interested in working in Germany, please see remote location comments later on as well as to my whys for looking around.',
+    employment: 'Perfect.',
     startdate: 'As soon as possible is great, I do however have a 2 month notice period. As they say, good things come to those who wait.',
     salary: 'Without beating around the bush, this is quite a it below my range and experience. Negotiation is on the table, I know that I can bring more than you planned for, resulting in a win-win. What about some part in ETH? (Saves me investing each month via the Fiat -> BTC -> ETH bridges)',
     industry: 'I have been involved with Bitcoin as an buyer since late 2012 (having dimissed it totally in 2010). As for Ethereum, actually stumbled across it only in Jan 2016 after I wanted to write some Bitcoin gambling site... Well, I have been hooked since, contracts, interfaces - not enough to satisfy the learning appetite, but enough to keep me sane.',
-    companysize: 'Brilliant. Be small, move fast, do things that matter.'
+    companysize: 'Brilliant. Be small, move fast, do things that matter.',
+    teamsize: 'I would expect a minium of 2 (obsiously depends on where the process is), single-points of failures are not advisable.'
   },
   methodology: {
+    codereviews: '100%, nobody knows everything and alignment is critical.',
+    prototyping: 'Along with failfast, rather invest small block of time before jumping in heads over heels.',
+    pairprogramming: 'Two heads are better than one.',
+    failfast: 'On-board.',
+    unittests: 'This is actually one of the things that attracts me (having followed Parity on GithHub since the public annoucement) - quality is being taken seriously.',
     buildserver: 'Have a lot of experience with CircleCI (slightly cheaper for paid accounts than Travis), however with this project took the Travis route. Different configuration and usage options in the yml config, however a CI is a CI...',
     staticcodeanalysis: 'For JavaScript CodeClimate does work wonders, although (by default) they rely on jshint as opposed to eslint (their engine config is not quite optimal yet for the latter). Combine that with a local ESLint and coding can be checked for the simpler issues.',
-    onecommandbuild: 'Unfortunately too many in this space leave out rigour when building, yes, a proper build (HTML, JS, CSS, etc.) is critical. Very happy to see this here.'
+    onecommandbuild: 'Unfortunately too many in this space leave out rigour when building, yes, a proper build (HTML, JS, CSS, etc.) is critical. Very happy to see this here. It may be \'just JS\', but it is coding, best-practices should apply.'
   },
   equipment: {
     operatingsystem: 'Currently a Mac OS X user. Prior to that have been using Linux as a desktop OS since 1996, however when I finally got my hands on a Mac Mini G5 with BSD under-the-hood, I have not looked back.',
@@ -20,7 +27,13 @@ module.exports = {
     html5: 'Wider than just the additional tags introduced, HTML5 still has some way to grow for exotic features - we live in interesting times. The challenge is more around not what you should use, but rather what you should not use',
     javascript: 'We have come a long way and I would have never expected myself to use JS on both the server and the client, but here we are. It does have its moments, provided you apply some rigour and ES2015 has definately helped.',
     node: 'Yes. Current propritary projects I am involved in are all a combination of client/server JS, with a mixture of code-sharing between the sides. I never would have imagined this as a solid option in 1996 when I was knee deep in C++ as saw JS as a way to pop up alert boxes on the browser.',
+    rest: 'GET, POST, PUT - definately know my way around using and constructing REST APIs.',
+    jsonrpc: 'Not my favourite compared to REST (reminds me a bit of the SOAP days), however I also have an implementation from JS to the Ethereum JSONRPC APIs. Whatever works.',
+    uiux: 'Not the worlds best, however I know I have a touch here - blame a misspent youth with Delphi and an eye on detail.',
+    design: 'As the previous one, have an instinct for what works, although I will not be able to make my living purely as a designer.',
     testing: 'Mocha (server-side) and Mocha with Karma (client side) really rules the roost. For end-to-end integration Selenium (exposure via Angular\'s Protractor) does work wonders, although it can be quite difficult to maintain longer-term.',
-    frameworks: 'Biggest strength is on AngularJS (extensive 1.x, no production 2.x experience) and have been using Polymer and React (as in this project) lately to expand my horizons. Apart from my initial JSX knee-jerk, the latter has been quite good, not without its warts, but productive. Familiar with Meteor, however have never viewed it as a real-contender, rather as a slightly-less attractive approach.'
+    frameworks: 'Biggest strength is on AngularJS (extensive 1.x, no production 2.x experience) and have been using Polymer and React (as in this project) lately to expand my horizons. Apart from my initial JSX knee-jerk, the latter has been quite good, not without its warts, but productive. Familiar with Meteor, however have never viewed it as a real-contender, rather as a slightly-less attractive approach.',
+    p2p: 'I have not had the pleasure of writing my own p2p implementation yet, however understand how, why and how, be it Napster, Bittorrent or Whisper.',
+    ethereum: ''
   }
 };
