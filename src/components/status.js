a3eb013c9f037370df5056180387e952b9a28590
Jaco Greeff <jacogr@gmail.com>
2016-04-27 12:21:09 +0200

    expanded comments

--- src/components/status.js
+++ src/components/status.js
@@ -7,7 +7,7 @@ module.exports = {
     companysize: 'Brilliant. Be small, move fast, do things that matter.'
   },
   methodology: {
-    buildserver: 'Have a lot of experience with CircleCI (slightly cheaper for paid accounts than Travis), however with this project took the Travis route. Slightly different syntaxin the yml config, however a CI is a CI...',
+    buildserver: 'Have a lot of experience with CircleCI (slightly cheaper for paid accounts than Travis), however with this project took the Travis route. Different configuration and usage options in the yml config, however a CI is a CI...',
     staticcodeanalysis: 'For JavaScript CodeClimate does work wonders, although (by default) they rely on jshint as opposed to eslint (their engine config is not quite optimal yet for the latter). Combine that with a local ESLint and coding can be checked for the simpler issues.',
     onecommandbuild: 'Unfortunately too many in this space leave out rigour when building, yes, a proper build (HTML, JS, CSS, etc.) is critical. Very happy to see this here.'
   },
@@ -16,10 +16,11 @@ module.exports = {
     computer: 'Thin, light, portable with enough memory and a long battery life.'
   },
   technologies: {
-    css3: 'I can hold my own.',
-    html5: '',
+    css3: 'I can hold my own. As with the rest in this section, an \'Expert\' label always leads me to being uncomfortable - there are always people better (I have seen some real wizards) and the more you know, the more you reliase you still need to learn.',
+    html5: 'Wider than just the additional tags introduced, HTML5 still has some way to grow for exotic features - we live in interesting times. The challenge is more around not what you should use, but rather what you should not use',
     javascript: 'We have come a long way and I would have never expected myself to use JS on both the server and the client, but here we are. It does have its moments, provided you apply some rigour and ES2015 has definately helped.',
-    node: 'Yes. Current propritary projects are all a combination of client/server JS, with a mixture of code-sharing.',
-    frameworks: 'Biggest strength is on AngularJS (1.x, no production 2.x experience) and have been using Polymer and React (like here) lately to expand my horizons. Apart from my initial JSX knee-jerk, the latter has been quite good, not without its warts, but productive. Familiar with Meteor, however have never viewed it as a real-contender, rather as a slightly-less attractive approach.'
+    node: 'Yes. Current propritary projects I am involved in are all a combination of client/server JS, with a mixture of code-sharing between the sides. I never would have imagined this as a solid option in 1996 when I was knee deep in C++ as saw JS as a way to pop up alert boxes on the browser.',
+    testing: 'Mocha (server-side) and Mocha with Karma (client side) really rules the roost. For end-to-end integration Selenium (exposure via Angular\'s Protractor) does work wonders, although it can be quite difficult to maintain longer-term.',
+    frameworks: 'Biggest strength is on AngularJS (extensive 1.x, no production 2.x experience) and have been using Polymer and React (as in this project) lately to expand my horizons. Apart from my initial JSX knee-jerk, the latter has been quite good, not without its warts, but productive. Familiar with Meteor, however have never viewed it as a real-contender, rather as a slightly-less attractive approach.'
   }
 };
