aff7c1187c06c8bb08d8a65027663d34d8fc5da0
Jaco Greeff <jacogr@gmail.com>
2016-04-28 09:25:09 +0200

    formatting key overrides

--- src/mixins/section.spec.js
+++ src/mixins/section.spec.js
@@ -37,4 +37,29 @@ describe('mixins/section', () => {
       expect(section.formatTime(1234)).to.equal('12h34');
     });
   });
+
+  describe('formatEntry', () => {
+    describe('key overrides', () => {
+      it('formats startdate', () => {
+        expect(section.formatEntry('startdate', 1461827495755)).to.equal('28th april 2016');
+      });
+
+      it('formats amount', () => {
+        expect(section.formatEntry('amount', 12345678)).to.equal('12,345,678');
+      });
+
+      it('formats currency', () => {
+        expect(section.formatEntry('currency', 'EUR')).to.equal('eur');
+      });
+
+      it('formats from and to', () => {
+        expect(section.formatEntry('from', '0800')).to.equal('08h00');
+        expect(section.formatEntry('to', '1800')).to.equal('18h00');
+      });
+
+      it('formats the workweek', () => {
+        expect(section.formatEntry('workweek', 40 * 60 * 60)).to.equal('40h');
+      });
+    });
+  });
 });
