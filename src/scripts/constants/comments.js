4964129f29c7c8333702ab6a67d4b51b04bf2f6d
Jaco Greeff <jacogr@gmail.com>
2016-04-29 08:15:16 +0200

    spelling fixes

--- src/scripts/constants/comments.js
+++ src/scripts/constants/comments.js
@@ -7,17 +7,17 @@ module.exports = {
     salary: 'Without beating around the bush, this is quite a it below my range and experience. Negotiation is on the table, I know that I can bring more than you planned for, resulting in a win-win. What about some part in ETH?',
     industry: 'I have been involved with Bitcoin as an buyer since late 2012 (having dimissed it totally in 2010). As for Ethereum, actually stumbled across it only in Jan 2016 after I wanted to write some Bitcoin gambling site... Well, I have been hooked since, contracts, interfaces - not enough to satisfy the learning appetite, but enough to keep me sane.',
     companysize: 'Brilliant. Be small, move fast, do things that matter.',
-    teamsize: 'I would expect a minium of 2 (obsiously depends on where the process is), single-points of failures are not advisable.'
+    teamsize: 'I would expect a minimum of 2 (obsiously depends on where the process is), single-points of failures are not advisable.'
   },
   methodology: {
-    codereviews: '100%, nobody knows everything and alignment is critical.',
+    codereviews: '100%, nobody knows everything, learning, standards and alignment is critical.',
     prototyping: 'Along with failfast, rather invest small block of time before jumping in heads over heels.',
     pairprogramming: 'Two heads are better than one.',
     failfast: 'On-board.',
-    unittests: 'This is actually one of the things that attracts me (having followed Parity on GithHub since the public annoucement) - quality is being taken seriously throughout.',
+    unittests: 'This is actually one of the things that attracts me (having followed Parity on GithHub since the public announcement) - quality is being taken seriously throughout.',
     integrationtests: 'The inputs/outputs are always the fragile systems parts.',
     buildserver: 'Have a lot of experience with CircleCI (slightly cheaper for paid accounts than Travis), however with this project took the Travis route. Different configuration and usage options in the yml config, however a CI is a CI...',
-    staticcodeanalysis: 'For JavaScript CodeClimate does work wonders, although (by default) they rely on jshint as opposed to eslint (their engine config is not quite optimal yet for the latter). Combine that with a local ESLint/Sass-Lint/Pug-Lint and coding can be checked for the multiple style and other issues.',
+    staticcodeanalysis: 'For JavaScript CodeClimate does work wonders, although their engine config is not quite optimal yet for JS, relying on an older forked ESLint version. Combine that with a local ESLint/Sass-Lint/Pug-Lint and coding can be checked for multiple style, use and other issues.',
     versioncontrol: 'Not expecting anything else.',
     issuetracker: 'Especially with Kanban, other lightweight tools (where not interfacing with the public), could be useful, e.g. Trello or PivotalTracker.',
     knowledgerepo: 'Perfect.',
@@ -25,7 +25,7 @@ module.exports = {
     qaprotocol: 'I do like the approach followed with Parity, less garbage in, less garbage out.',
     agilemanagement: 'No frills overhead, allowing for greater focus.',
     freedomovertools: 'Some are more productive in vim, some in emacs and yet others using nothing more than a toaster.',
-    onecommandbuild: 'Unfortunately too many in this space leave out rigour when building, yes, a proper build (HTML, JS, CSS, etc.) is critical. Very happy to see this here. It may be \'just JS\', but it is coding, best-practices should apply.',
+    onecommandbuild: 'Unfortunately too many in this space leave out rigour when building. Yes, a proper build (HTML, JS, CSS, etc.) is critical. Very happy to see this here. It may be \'just JS\', but it is coding, best-practices should apply.',
     quickstart: 'Drinking from the fire hydrant that is Ethcore.',
     commitondayone: 'Perfect.'
   },
@@ -35,26 +35,26 @@ module.exports = {
     holidays: 'On-par.',
     corehours: 'Makes sense, catering for both the early risers and later sleepers.',
     travel: 'When the travel makes sense from a business perspective, the fit is there.',
-    remote: 'I am currently working in South Africa with a view to move to Germany to join my girfriend and new baby daughter. Most things you can do via the internet (I am well-versed in remote work from Germany, Canada and other places), being a proper parent is not one of them. Currently we are open to locations, however current location in Germany is closer to Amsterdam than Berlin.',
+    remote: 'I am currently working in South Africa with a view to move to Germany to join my girfriend and new baby daughter. Most things you can do via the internet (I am well-versed in remote work from Germany, Canada and other places), being a proper parent is not one of them. Currently we are open to locations, however our current location in Germany is closer to Amsterdam than Berlin.',
     relocationpackage: 'Not needed.'
   },
   profile: {
-    newfeatures: 'Happy to see this here as (currently) the biggest area. I am better changing the world radiacally, than doing it incrementally.',
+    newfeatures: 'Happy to see this here as (currently) the biggest area. I am better changing the world radically, than doing it incrementally.',
     clientsupport: 'The only way to truely learn is from clients. Without them pushing through feedback and without them using the products, we have nothing. The closer you are to your clients (in my experience), the better the actual outputs.',
     documentation: 'Exceptionally important, however not always the most plesant task to complete.',
     maintenance: 'This will increase over time.',
     meetings: 'Good starting point.'
   },
   equipment: {
-    operatingsystem: 'Currently a Mac OS X user. Prior to that have been using Linux as a desktop OS since 1996, however when I finally got my hands on a Mac Mini G5 with BSD under-the-hood, I have not looked back.',
+    operatingsystem: 'Currently a Mac OS X user. Prior to that have been using Linux as a desktop OS since 1996. When I finally got my hands on a Mac Mini G5 with BSD under-the-hood, I have not looked back.',
     computer: 'Thin, light, portable with enough memory and a long battery life.',
     monitors: 'A single external screen is fine, for some reason I always default to using just the laptop display.'
   },
   technologies: {
-    css3: 'I can hold my own. As with the rest in this section, an \'Expert\' label always leads me to being uncomfortable - there are always people better (I have seen some real wizards) and the more you know, the more you reliase you still need to learn.',
+    css3: 'I can hold my own. As with the rest in this section, an \'Expert\' label always leads me to being uncomfortable - there are always people better (I have seen some real wizards) and the more you know, the more you realise you still need to learn.',
     html5: 'Wider than just the additional tags introduced, HTML5 still has some way to grow for exotic features - we live in interesting times. The challenge is more around not what you should use, but rather what you should not use',
-    javascript: 'We have come a long way and I would have never expected myself to use JS on both the server and the client, but here we are. It does have its moments, provided you apply some rigour and ES2015 has definately helped.',
-    node: 'Yes. Current propritary projects I am involved in are all a combination of client/server JS, with a mixture of code-sharing between the sides. I never would have imagined this as a solid option in 1996 when I was knee deep in C++ as saw JS as a way to pop up alert boxes on the browser.',
+    javascript: 'We have come a long way and I would have never expected myself to use JS on both the server and the client, but here we are. It does have its moments, provided you apply some rigour and ES2015 has definately helped. Frameworks are a bit of a wild wild west, but it is improving.',
+    node: 'Yes. Current propritary projects I am involved in are all a combination of client/server JS, with a mixture of code-sharing between the sides. I never would have imagined this as a solid option in 1996 when I was knee deep in C++ and only saw JS as a way to pop up alert boxes on the browser.',
     rest: 'GET, POST, PUT - definately know my way around using and constructing REST APIs.',
     jsonrpc: 'Not my favourite compared to REST (reminds me a bit of the SOAP days), however I also have an implementation from JS to the Ethereum JSONRPC APIs. Whatever works.',
     uiux: 'Not the worlds best, however I know I have a touch here - blame a misspent youth with Delphi and an eye on detail.',
@@ -64,14 +64,14 @@ module.exports = {
     p2p: 'I have not had the pleasure of writing my own p2p implementation yet, however understand how, why and how, be it Napster, Bittorrent or Whisper.',
     ethereum: 'Yes, yes and yes. Make no mistake, I am no Vitalik, but still believe this is going to change the world around us. I guess I took a crypto break at exactly the wrong time, I wish I got involved even earlier.',
     blockchain: 'As a distributed Excel, I am familiar with it, from a coding perspective I only have experience with Ethereum contracts, the Ethereum RPC interfaces and most things Ethereum related.',
-    server: 'I used to be a C++ advocate in the days when interpreted languages and garbage collection was still frowned-upon. In addition I am familiar with Ruby (not Rails as much) and used to be able to wrangle the Python serpernt with the best of them. Alas, no-use have gotten in the way. Rust and Go are on my 2016 \'learn and play with\' list, sadly I am a bit behind since I got into Ethereum - and there is only so much time to play.',
+    server: 'I used to be a C++ developer in the days when interpreted languages and garbage collection was still frowned-upon. In addition I am familiar with Ruby (not Rails as much) and used to be able to wrangle the Python serpent with the best of them. Alas, no-use have gotten in the way. Rust and Go are on my 2016 \'learn and play with\' list, sadly I am a bit behind since I got into Ethereum - and there is only so much time to play.',
     gametheory: 'I am familiar with the basics (I love reading), however from a pure mathematical viewpoint, I am nowhere. Yes, a lot of this forms the backbone of the industry, so I guess my knowledge is just enought to be dangerous.',
-    cryptography: 'Once again, no expert. I know enough that I am not duped by Ethereum using Keccak256 (as opposed to SHA3), know where to find best-practices (and why it is a bad idea to try and implement this yourself). Concious incompetence, I do not have the skills or background to be hardcore in this.',
+    cryptography: 'Once again, no expert. I know enough that I am not duped by Ethereum using Keccak256 (as opposed to SHA3), know where to find best-practices and why it is a bad idea to try and implement this yourself. Conscious incompetence, I do not have the skills or background to be hardcore in this.',
     boardgames: 'The tales of a misspent youth. Here I only put myself on the casual track, if I had to work on the AlphaGo team, I would not be an MVP.'
   },
   other: 'Changing the world is exactly the reason why I want to sign up - work where you can have the largest impact. When something is worth doing, it is worth doing right. If there is space for a new Vibram-wearing member of the team, count me in.',
   misc: {
-    training: 'My view is that this is the best approach, I have yet to receive weeks-long formal training that yielded the same outputs as the inputs.',
+    training: 'My view is that this is the best approach, I have yet to receive weeks-long formal training that yielded the same outputs as the time inputs.',
     fossphilosophy: 'I have seen this approach with Parity and must say that it does seem to be working exceptionally well.',
     codingretreats: 'A fair warning - I am both a terrible snowboarder and a terrible skier, some things are better learnt when in your 20\'s.',
     conferences: 'If it adds value to the day-to-day and the future, I am all-in.',
